a9f1916455b817ab8a97433dc7b570524a65590b
contaria <kingcontaria@gmail.com>
2025-07-10 02:56:20 +0200

    fix: correct deep ocean biome layer

--- src/main/java/me/contaria/glacier/mixin/gen/biome_layers/AddDeepOceanLayerMixin.java
+++ src/main/java/me/contaria/glacier/mixin/gen/biome_layers/AddDeepOceanLayerMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.world.biome.layer.type.CrossSamplingLayer;
 import net.minecraft.world.biome.layer.util.LayerSampleContext;
 import net.minecraft.world.biome.layer.util.LayerSampler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 
 import static net.minecraft.world.biome.layer.BiomeLayers.*;
 
@@ -15,12 +14,20 @@ public abstract class AddDeepOceanLayerMixin implements CrossSamplingLayer {
     @Override
     public int sample(LayerSampleContext<?> context, LayerSampler parent, int x, int z) {
         int center = parent.sample(this.transformX(x + 1), this.transformZ(z + 1));
-        if (isShallowOcean(center) && this.isSurroundedByEnoughShallowOcean(parent, x, z)) {
+        if (isShallowOcean(center) &&
+                isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z))) &&
+                isShallowOcean(parent.sample(this.transformX(x + 2), this.transformZ(z + 1))) &&
+                isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z + 2))) &&
+                isShallowOcean(parent.sample(this.transformX(x), this.transformZ(z + 1)))
+        ) {
             if (center == WARM_OCEAN_ID) {
                 return DEEP_WARM_OCEAN_ID;
             }
             if (center == LUKEWARM_OCEAN_ID) {
                 return DEEP_LUKEWARM_OCEAN_ID;
+            }
+            if (center == OCEAN_ID) {
+                return DEEP_OCEAN_ID;
             }
             if (center == COLD_OCEAN_ID) {
                 return DEEP_COLD_OCEAN_ID;
@@ -31,16 +38,5 @@ public abstract class AddDeepOceanLayerMixin implements CrossSamplingLayer {
             return DEEP_OCEAN_ID;
         }
         return center;
-    }
-
-    @Unique
-    private boolean isSurroundedByEnoughShallowOcean(LayerSampler parent, int x, int z) {
-        if (isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z)))) {
-            if (isShallowOcean(parent.sample(this.transformX(x + 2), this.transformZ(z + 1)))) {
-                return isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z + 2))) || isShallowOcean(parent.sample(this.transformX(x), this.transformZ(z + 1)));
-            }
-            return isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z + 2))) && isShallowOcean(parent.sample(this.transformX(x), this.transformZ(z + 1)));
-        }
-        return isShallowOcean(parent.sample(this.transformX(x + 2), this.transformZ(z + 1))) && isShallowOcean(parent.sample(this.transformX(x + 1), this.transformZ(z + 2))) && isShallowOcean(parent.sample(this.transformX(x), this.transformZ(z + 1)));
     }
 }
